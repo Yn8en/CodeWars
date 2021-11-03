d7b60a3f7a164b2fe67c66f7937ab35a17c1ed0b
Timo Hartmann <timo.hartmann.business@gmail.com>
2021-11-03 21:20:25 +0100

    some refactoring, distilled down ... no breaks

--- buddies.js
+++ buddies.js
@@ -7,67 +7,42 @@ function buddy(start, limit) {
     }
 
     class MemoList {
-        constructor(int=[]) {
-            this[1]=new IntegerInfo(1); // init basecases, simplicity
-            this[2]=new IntegerInfo(2); // init basecases, simplicity
-            try { // in case constructor is handed over an initial object from JSON or whatever
-                for (let key in int) {this.addMemo(key)}
-            } catch (error) {
-                console.log(error);
-            }
-        }
-        
-        addMemo(int, baseDivsA=[1], multipleA=1, baseDivsB=[1], multipleB=1) {
+        addMemo(int) {
             if (int in lookup) { // MEMO in lookup or basecase => rebounce
-                return; //rebounce! => BASECASE or MEMO hit!
-            }
-            // in case addMemo for int was called with sufficient info about divisors down the tree => actually append new memo!
-            if (multipleA>1 && multipleB>1) {
-                this[int] = new IntegerInfo(int, baseDivsA, multipleA,baseDivsB, multipleB);
                 return;
             }
-            //assume prime generally, unless found a pair of divisors
-            let prime=true;
-            //loop through all possible divisors. so that => int%div = 0
-            //could be sped up by breaking loop on first hit, but I will stick with it!
+
             for(let i=2; i<=(Math.sqrt(int)); i++) {
                 if (int%i === 0) {
                     this.addMemo(int/i);
                     this.addMemo(i);
-                    this.addMemo(int, this[i].divs, int/i, this[int/i].divs, i);
-                    prime=false; // found divisors, toggle prime!
+                    this[int] = new IntegerInfo(int, [this[i].divs, int/i], [this[int/i].divs, i]);
+                    return;
                 }
             }
-            // poor lonely prime numbers without buddies!
-            if (prime) {
-                this[int] = new IntegerInfo(int);                
-            }
+            this[int] = new IntegerInfo(int);                
         }
     }
     
     // IntegerInfo object, holding divisors and buddy for each int!
     class IntegerInfo {
-        constructor (int, baseDivsA=[1], multipleA=1, baseDivsB=[1], multipleB = 1) {
+        constructor (int, [baseDivsA, multipleA]=[[1],1], [baseDivsB, multipleB]=[[1],1]) {
             if (multipleA===1) {
                 this.divs=[1];
                 this.myBelovedBuddy=0;
             } else {
                 const uniqueify = (value, index, self) => (self.indexOf(value) === index);
                 const sumValues = (obj) => (obj.reduce((a, b) => (a + b)));
-
-                let array = [...baseDivsA, ...baseDivsB, ...baseDivsA.map(x=>x*multipleA), ...baseDivsB.map(x=>x*multipleB)];
-                this.divs = new Float64Array(array.filter(uniqueify)).sort();
+                let divArray = [...baseDivsA, ...baseDivsB, ...baseDivsA.map(x=>x*multipleA), ...baseDivsB.map(x=>x*multipleB)];
+                this.divs = new Float64Array(divArray.filter(uniqueify)).sort();
                 this.myBelovedBuddy = sumValues(this.divs)-1;
             }
         }
     }
     
     function solve (from, upTo) {
-        let chkBuddy;
-        let buddysBuddy;
-
         // optional, not needed, but somehow it speeds things up a tiny bit
-        // example - calling: buddy(28457, 859856) with timeLog wrapped around:
+        // example - calling: buddy(28457, 859856) ... timeLog wrapped:
         //                     result: [ 62744, 75495 ]
         //   running preparation loop: duration: 764.293ms
         //  skipping preparation loop: duration: 904.422ms
@@ -75,16 +50,19 @@ function buddy(start, limit) {
             lookup.addMemo(i);
         }
 
+        let chkBuddy;
+        // core solver loop!
         for (let i=from; i<=upTo; i++) {
             lookup.addMemo(i);
             chkBuddy = lookup[i].myBelovedBuddy;
-            lookup.addMemo(chkBuddy);
-            buddysBuddy = lookup[chkBuddy].myBelovedBuddy;
-            if (buddysBuddy == i) {
-                console.timeLog("duration:");
-                return [i, chkBuddy];
+            if (chkBuddy!=0) {
+                lookup.addMemo(chkBuddy);
+                if (lookup[chkBuddy].myBelovedBuddy == i) {
+                    console.timeLog("duration:");
+                    console.log(lookup);
+                    return [i, chkBuddy];
+                }
             }
-
         }
         console.timeLog("duration:");
         return "Nothing";
@@ -94,4 +72,4 @@ function buddy(start, limit) {
     let lookup = new MemoList;
     return solve(start,limit);
 }
-console.log(buddy(28457, 859856));
+console.log(buddy(1, 50));
