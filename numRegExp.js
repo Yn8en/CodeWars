e25ba4d1295c7e9df4db5a0afcfb51eaa10d3941
Timo Hartmann <timo.hartmann.business@gmail.com>
2021-11-03 21:20:59 +0100

    refactoring, removed 2nd version of func

--- numRegExp.js
+++ numRegExp.js
@@ -1,24 +1,13 @@
 function generateRegex(charClasses, minLength) {
-    console.log(minLength);
-    console.log(charClasses);
     let accumulator="^.*";
-    let allowed="(";
-    // accumulate regex core for all charClasses
     for (let i=0; i<charClasses.length; i++) {
-        // build part for each charClass
-        // while at it, accumulate "allowed" string as well;
         accumulator+="(?=";
         for (let j=0; j<charClasses[i][1]; j++) {
-            // console.log("appended nr. "+(j+1)+" of "+charClasses[i][1]+" total!");
-            accumulator+=".*"+charClasses[i][0]
+            accumulator+=".*"+charClasses[i][0];
         }
         accumulator+=")";
-        allowed+=charClasses[i][0]+"|";
     }
-    allowed= allowed.slice(0, -1).concat("|[A-Za-z0-9])");
-    
-    //add length restriction and append allowed chars as "or"-group & close
-    accumulator+="(?=.{"+minLength+",})"+allowed+"+$"
+    accumulator+="(?=.{"+minLength+",}).*$"
     let regex = new RegExp(accumulator)
     console.log(regex);
     return regex;
@@ -27,32 +16,5 @@ function generateRegex(charClasses, minLength) {
 // example - functionality testing with https://www.regextester.com/
 let charClass = [["[-_.!#@§$%&/()\"\\]",3],["[A-Z]",3],["[0-9]",3]];
 let minLength = 12;
-
 let password = "Bruder!Muss@Los#1876";
 console.log(generateRegex(charClass, minLength).test(password));
-
-
-
-function generateRegex2(charClasses, minLength) {
-    let accumulator="^.*";
-    let allowed="(";
-    // accumulate regex core for all charClasses
-    for (let i=0; i<charClasses.length; i++) {
-        // build part for each charClass
-        // while at it, accumulate "allowed" string as well;
-        accumulator+="(?=";
-        for (let j=0; j<charClasses[i][1]; j++) {
-            // console.log("appended nr. "+(j+1)+" of "+charClasses[i][1]+" total!");
-            accumulator+=".*"+charClasses[i][0]
-        }
-        accumulator+=")";
-        allowed+=charClasses[i][0]+"|";
-    }
-    allowed= allowed.slice(0, -1).concat(")");
-    
-    //add length restriction and append allowed chars as "or"-group & close
-    accumulator+="(?=.{"+minLength+",}).*$" // ".*$" => allowed+"+$", in case!
-    let regex = new RegExp(accumulator)
-    console.log(regex);
-    return regex;
-}
