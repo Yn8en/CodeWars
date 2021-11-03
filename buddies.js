c132e846256b0fec19b2481f27be0b70a0150a14
Timo Hartmann <timo.hartmann.business@gmail.com>
2021-11-03 19:49:15 +0100

    Fixed Bugs, rounded up, added timeLog

--- buddies.js
+++ buddies.js
@@ -1,11 +1,16 @@
+//? https://www.codewars.com/kata/59ccf051dcc4050f7800008f
+
 function buddy(start, limit) {
-    // MemoList Object, containing all calculated integer objects
-    // ...may come handy to add some methods lateron
+    console.time("duration:");
+    if (start > limit) {
+        return "";
+    }
+
     class MemoList {
-        constructor(int =[]) {
-            this[1]=new IntegerInfo(1);
-            this[2]=new IntegerInfo(2);
-            try {
+        constructor(int=[]) {
+            this[1]=new IntegerInfo(1); // init basecases, simplicity
+            this[2]=new IntegerInfo(2); // init basecases, simplicity
+            try { // in case constructor is handed over an initial object from JSON or whatever
                 for (let key in int) {this.addMemo(key)}
             } catch (error) {
                 console.log(error);
@@ -13,36 +18,34 @@ function buddy(start, limit) {
         }
         
         addMemo(int, baseDivsA=[1], multipleA=1, baseDivsB=[1], multipleB=1) {
-            if (int in lookup) {
-                console.log("MEMO FOUND: "+ int);
+            if (int in lookup) { // MEMO in lookup or basecase => rebounce
                 return; //rebounce! => BASECASE or MEMO hit!
             }
-            console.log("CALLING addMemo with int: "+int+" __________________________")
+            // in case addMemo for int was called with sufficient info about divisors down the tree => actually append new memo!
             if (multipleA>1 && multipleB>1) {
-                console.log("Appending new Info int: "+int+" + [" + baseDivsA + "] * "+multipleA+"  +  [" +baseDivsB+ "]* "+multipleB);
                 this[int] = new IntegerInfo(int, baseDivsA, multipleA,baseDivsB, multipleB);
-                console.log("Appended    "+int+" => "+this[int].divs+"   and Buddy: "+this[int].myBelovedBuddy);
                 return;
             }
+            //assume prime generally, unless found a pair of divisors
             let prime=true;
-            for(let i=2; i<(Math.sqrt(int)); i++) {
+            //loop through all possible divisors. so that => int%div = 0
+            //could be sped up by breaking loop on first hit, but I will stick with it!
+            for(let i=2; i<=(Math.sqrt(int)); i++) {
                 if (int%i === 0) {
                     this.addMemo(int/i);
                     this.addMemo(i);
-                    //int, and 2x pairs [divs & factor down] taken from both above init memos!
                     this.addMemo(int, this[i].divs, int/i, this[int/i].divs, i);
-                    prime=false;
+                    prime=false; // found divisors, toggle prime!
                 }
             }
+            // poor lonely prime numbers without buddies!
             if (prime) {
-                console.log("Appending new Info for PRIME: "+int+" => divs [1] + multiple=1");
                 this[int] = new IntegerInfo(int);                
             }
         }
     }
     
-    // IntegerInfo object, holding the memoized info info for a given Integer
-    // myBelovedBuddy >> divSum ... much better suited to check buddies later!!
+    // IntegerInfo object, holding divisors and buddy for each int!
     class IntegerInfo {
         constructor (int, baseDivsA=[1], multipleA=1, baseDivsB=[1], multipleB = 1) {
             if (multipleA===1) {
@@ -51,53 +54,44 @@ function buddy(start, limit) {
             } else {
                 const uniqueify = (value, index, self) => (self.indexOf(value) === index);
                 const sumValues = (obj) => (obj.reduce((a, b) => (a + b)));
-                let incA = int/multipleA;
-                let incB = int/multipleB;
-                this.divs = new Float64Array([incA, incB, ...baseDivsA, ...baseDivsB, ...baseDivsA.map(x=>x*multipleA), ...baseDivsB.map(x=>x*multipleB)].filter(uniqueify)).sort();
-                console.log("pushed: " + int + " => divs: " + this.divs)
+
+                let array = [...baseDivsA, ...baseDivsB, ...baseDivsA.map(x=>x*multipleA), ...baseDivsB.map(x=>x*multipleB)];
+                this.divs = new Float64Array(array.filter(uniqueify)).sort();
                 this.myBelovedBuddy = sumValues(this.divs)-1;
             }
         }
     }
-    // initialize new MemoList as "lookup"
-    let lookup = new MemoList;    
     
-    // start,limit
     function solve (from, upTo) {
         let chkBuddy;
         let buddysBuddy;
+
+        // optional, not needed, but somehow it speeds things up a tiny bit
+        // example - calling: buddy(28457, 859856) with timeLog wrapped around:
+        //                     result: [ 62744, 75495 ]
+        //   running preparation loop: duration: 764.293ms
+        //  skipping preparation loop: duration: 904.422ms
+        for (let i = 1; i<from; i++) {
+            lookup.addMemo(i);
+        }
+
         for (let i=from; i<=upTo; i++) {
-            lookup.addMemo(i);  // check 100
-            // addMemo-> 100, [1,2,4,5,10,20,50], 92, 91
-            chkBuddy = lookup[i].myBelovedBuddy; // = 91
-            lookup.addMemo(chkBuddy); // check 91
-            // addMemo-> 91, [1, 7, 13], 21, 20
-            buddysBuddy = lookup[chkBuddy].myBelovedBuddy; // = 20
-            if (buddysBuddy == i) { // check if 100 = 20
+            lookup.addMemo(i);
+            chkBuddy = lookup[i].myBelovedBuddy;
+            lookup.addMemo(chkBuddy);
+            buddysBuddy = lookup[chkBuddy].myBelovedBuddy;
+            if (buddysBuddy == i) {
+                console.timeLog("duration:");
                 return [i, chkBuddy];
             }
+
         }
+        console.timeLog("duration:");
         return "Nothing";
     }
-    
-    let result = solve(start,limit);
-    console.log(lookup);
-    return result;
 
-    function runRandomTest(n, min, max) {
-        for (let i=1; i<n; i++) {
-            let int = Math.floor(min+Math.random()*(max-min));
-            console.log(int);
-            lookup.addMemo(int); 
-        }
-        return lookup;
-    }
-    // console.log(Object.keys(runRandomTest(50, 1, 1000)))
+    //actually running
+    let lookup = new MemoList;
+    return solve(start,limit);
 }
-console.log(buddy(4,100));
-
-// Task
-
-// Given two positive integers start and limit, the function buddy(start, limit) should return the first pair (n m) of buddy pairs such that n (positive integer) is between start (inclusive) and limit (inclusive); m can be greater than limit and has to be greater than n
-
-// If there is no buddy pair satisfying the conditions, then return "Nothing" or (for Go lang) nil or (for Dart) null; (for Lua, Pascal, Perl) [-1, -1].
+console.log(buddy(28457, 859856));
