4fe69781e3dddcc9c004d617b19ac8201420657d
Timo Hartmann <timo.hartmann.business@gmail.com>
2021-11-09 16:51:24 +0100

    some creativity!

---
+++ decayOfHumanity.js
@@ -0,0 +1,15 @@
+let beginDecayOfHumanity = (soon) => {
+    const problem = soon-1;
+    let now = new Array(soon).fill(["with","problems"]);
+
+    let creativity = (have, a, chance, though) => {
+        if (problem == chance) {
+            return "Hello World!"
+        }
+    }
+
+    let us = now.reduce(creativity);
+    return us;
+}
+
+console.log(beginDecayOfHumanity(69));
